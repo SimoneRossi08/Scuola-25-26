1939ef4f6fa1eba016acf843f7b264b1041e10c6
Simone Rossi <simone.rossi@galileiostiglia.edu.it>
2025-10-09 10:25:10 +0000

    09/10/2025

---
+++ Materie/Informatica/Esercizi/Lezioni/string.c
@@ -0,0 +1,34 @@
+#include "string.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * 
+ */
+
+string crstringdim(int n){
+
+    string c=(string)malloc(n*sizeof(char));
+    if(c==NULL){
+        printf("Errore");
+        exit(1);
+    }
+    fgets(c, n, stdin);
+
+    return c;
+}
+
+/**
+ * 
+ */
+
+ int clclunstring(string c){
+
+    int dim=0;
+
+    while(c[dim]!='\0'){
+        dim++;
+    }
+
+    return dim;
+}
