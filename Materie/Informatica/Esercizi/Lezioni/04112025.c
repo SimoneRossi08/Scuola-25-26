f341182c80ada3046c99ec65352f7d0f25846996
Simone Rossi <simone.rossi@galileiostiglia.edu.it>
2025-11-04 09:50:20 +0000

    04/11/2025

--- Materie/Informatica/Esercizi/Lezioni/04112025.c
+++ Materie/Informatica/Esercizi/Lezioni/04112025.c
@@ -7,18 +7,24 @@
 typedef char* string;
 typedef enum {true, false} bool;
 
-string infoname(string name, string surname){
+string infoname(string name){
 
     name=malloc(name, 20*sizeof(char));
     printf("Inserisci il suo nome: ");
     fgets(name,stdin);
     name=reallo(rname, (strlen(name))*sizeof(char));
 
+    return name;
+}
+
+string infosurname(string surname){
+
     surname=malloc(surname, 20*sizeof(char));
     printf("Inserisci il suo cognome: ");
     fgets(surname,stdin);
     surname=reallo(surname, (strlen(surname))*sizeof(char));
 
+    return surname;
 }
 
 bool infosex(bool sex){
@@ -34,11 +40,64 @@ bool infosex(bool sex){
     else{
         return true;
     }
-
 }
-void fcode(string name, string surname, bool sex, int dd, int mm, int yy){
 
+int infomm(int mm){
 
+    int flag;
+    printf("Inserisci la sua data di nascita: ");
+
+    //mese
+    do{
+        printf("\nMese:");
+        scanf("%d", &mm);
+        if(mm>12 || mm<1){
+            printf("\nERRORE.");
+            flag=1;
+        }
+        else{
+            flag=0;
+        }
+    }while(flag!=0);
+    
+    return mm;
+}
+
+int infodd(int dd, int mm){
+
+    int flag;
+
+    //giorno
+    do{
+        printf("\nGiorno:");
+        scanf("%d", &dd);
+        if(dd>30 && mm!=1 && mm!=3 && mm!=5 && mm!=7 && mm!=8 && mm!=10 && mm!=12){
+            printf("\nERRORE.");
+            flag=1;
+        }
+        if(dd>31 && mm==1 && mm==3 && mm==5 && mm==7 && mm==8 && mm==10 && mm==12){
+            printf("\nERRORE.");
+            flag=1;
+        }
+        if(dd>29 && mm==2){
+            printf("\nERRORE.");
+            flag=1;
+        }
+        else{
+            flag=0;
+        }
+    }while(flag!=0);
+
+    return dd;
+}
+
+int infoyy(int yy){
+
+    //anno
+    printf("\nAnno:");
+    scanf("%d", &yy);
+
+    return yy;    
 }
 
 int main(){
@@ -48,6 +107,22 @@ int main(){
     bool sex;
     int dd,mm,yy;
 
+    string fcode;
+    string fcodename=infoname(name);
+    string fcodesurname=infosurname(surname);
+    string fcodemm=infomm(mm);
+    string fcodedd=infodd(dd,fcodemm);
+    string fcodeyy=infoyy(yy);
+    char fcodeCOM={'G186'};
 
+    // CCC NNN YYMDD LLLL 
+
+    fcode=strcat(fcodesurname,fcodename);
+    fcode=strcat(fcode,fcodeyy);
+    fcode=strcat(fcode,fcodemm);
+    fcode=strcat(fcode,fcodedd);
+    fcode=strcat(fcode,fcodeCOM);
+
+    printf("%s",fcode);
 
 }
