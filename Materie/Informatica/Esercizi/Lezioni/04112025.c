460a0aa4f024af270b25fdc9e3f1baee70f66d0c
Simone Rossi <simone.rossi@galileiostiglia.edu.it>
2025-11-06 12:57:39 +0000

    06/11/2025

--- Materie/Informatica/Esercizi/Lezioni/04112025.c
+++ Materie/Informatica/Esercizi/Lezioni/04112025.c
@@ -7,34 +7,59 @@
 typedef char* string;
 typedef enum {true, false} bool;
 
+int cnt;
 string infoname(string name){
 
-    name=malloc(name, 20*sizeof(char));
+    string nnn;
+    name=(string)malloc((20+1)*sizeof(char));
     printf("Inserisci il suo nome: ");
-    fgets(name,stdin);
-    name=reallo(rname, (strlen(name))*sizeof(char));
+    fgets(name,20,stdin);
+    name=(string)realloc(name, (strlen(name)+1)*sizeof(char));
 
-    return name;
+    for(int i=0; i<strlen(name); i++){
+        if(name[i]!='a'&&name[i]!='e'&&name[i]!='i'&&name[i]!='o'&&name[i]!='u'){
+            cnt++;
+        }
+    }
+    if(cnt>=4){
+        nnn[0]=name[0];
+        nnn[1]=name[1];
+        nnn[2]=name[2];
+    }
+
+    return nnn;
 }
 
 string infosurname(string surname){
 
-    surname=malloc(surname, 20*sizeof(char));
-    printf("Inserisci il suo cognome: ");
-    fgets(surname,stdin);
-    surname=reallo(surname, (strlen(surname))*sizeof(char));
 
-    return surname;
+    surname=(string)malloc((20+1)*sizeof(char));
+    printf("Inserisci il suo cognome: ");
+    fgets(surname,20,stdin);
+    surname=(string)realloc(surname, (strlen(surname)+1)*sizeof(char));
+
+    for(int i=0; i<strlen(surname); i++){
+        if(surname[i]!='a'&&surname[i]!='e'&&surname[i]!='i'&&surname[i]!='o'&&surname[i]!='u'){
+            cnt++;
+        }
+    }
+    if(cnt>=4){
+        ccc[0]=surname[0];
+        ccc[1]=surname[1];
+        cc[2]=surname[2];
+    }
+
+    return ccc;
 }
 
 bool infosex(bool sex){
 
-    string sex=malloc(sex, 20*sizeof(char));
+    string sSex=(string)malloc((20+1)*sizeof(char));
     printf("Inserisci il suo sesso: ");
-    fgets(sex,stdin);
-    sex=reallo(sex, (strlen(sex))*sizeof(char));
+    fgets(sSex,20,stdin);
+    sSex=(string)realloc(sSex, (strlen(sSex)+1)*sizeof(char));
 
-    if(sex[1]=('F')){
+    if(sSex[1]=('F')){
         return false;
     }
     else{
@@ -49,10 +74,10 @@ int infomm(int mm){
 
     //mese
     do{
-        printf("\nMese:");
+        printf("\nMese: ");
         scanf("%d", &mm);
         if(mm>12 || mm<1){
-            printf("\nERRORE.");
+            printf("\nERRORE. ");
             flag=1;
         }
         else{
@@ -69,18 +94,18 @@ int infodd(int dd, int mm){
 
     //giorno
     do{
-        printf("\nGiorno:");
+        printf("\nGiorno: ");
         scanf("%d", &dd);
         if(dd>30 && mm!=1 && mm!=3 && mm!=5 && mm!=7 && mm!=8 && mm!=10 && mm!=12){
-            printf("\nERRORE.");
+            printf("\nERRORE. ");
             flag=1;
         }
         if(dd>31 && mm==1 && mm==3 && mm==5 && mm==7 && mm==8 && mm==10 && mm==12){
-            printf("\nERRORE.");
+            printf("\nERRORE. ");
             flag=1;
         }
         if(dd>29 && mm==2){
-            printf("\nERRORE.");
+            printf("\nERRORE. ");
             flag=1;
         }
         else{
@@ -94,34 +119,33 @@ int infodd(int dd, int mm){
 int infoyy(int yy){
 
     //anno
-    printf("\nAnno:");
+    printf("\nAnno: ");
     scanf("%d", &yy);
 
     return yy;    
 }
 
+ 
 int main(){
 
     string name;
     string surname;
     bool sex;
     int dd,mm,yy;
-
+    char COM={'G186'};
     string fcode;
-    string fcodename=infoname(name);
-    string fcodesurname=infosurname(surname);
-    string fcodemm=infomm(mm);
-    string fcodedd=infodd(dd,fcodemm);
-    string fcodeyy=infoyy(yy);
-    char fcodeCOM={'G186'};
+
+    name=fcodename=infoname(name);
+    surname=infosurname(surname);
+    mm=infomm(mm);
+    dd=infodd(dd,fcodemm);
+    yy=infoyy(yy);
+
+    if(sex=false){
+        dd+=40;
+    }
 
     // CCC NNN YYMDD LLLL 
-
-    fcode=strcat(fcodesurname,fcodename);
-    fcode=strcat(fcode,fcodeyy);
-    fcode=strcat(fcode,fcodemm);
-    fcode=strcat(fcode,fcodedd);
-    fcode=strcat(fcode,fcodeCOM);
 
     printf("%s",fcode);
 
