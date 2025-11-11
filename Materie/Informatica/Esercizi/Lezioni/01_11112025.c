14c81c46758731e2b16a6f4f416360de767f1055
Simone Rossi <simone.rossi@galileiostiglia.edu.it>
2025-11-11 09:50:23 +0000

    11/11/2025

--- Materie/Informatica/Esercizi/Lezioni/01_11112025.c
+++ Materie/Informatica/Esercizi/Lezioni/01_11112025.c
@@ -1,27 +1,34 @@
-/* Cifratura */
+/* Data una stringa iniziale e il fattore di
+    cifratura creare la stringa cifrata */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-typedef char* string;
+//typedef char* string;
 
 int main(){
 
-    string cfr;
+    char cfr[50];
     int n;
 
 
     printf("\nInserisci una parola: ");
-    fgets(cfr, 30, stdin);
+    fgets(cfr, 20, stdin);
 
     printf("\nInserisci una cifra: ");
-    scanf("%d", &n);
+    scanf(" %d", &n);
 
-    for(int i=0; i<sizeof(cfr)/sizeof(char); i++){
-        cfr[i]+=n;
+    for(int i=0; i<strlen(cfr); i++){
+        cfr[i]=cfr[i]+n;
     }
 
-    printf("\n Ecco la parola cifrata. \n %s", cfr);
+    printf("\n Ecco la parola cifrata. \n %s \n", cfr);
 
+    for(int i=0; i<strlen(cfr); i++){
+        cfr[i]=cfr[i]-n;
+    }
+
+    printf("\n Ecco la parola decifrata. \n %s \n", cfr);
+    
     return 0;
 }
--- Materie/Informatica/Esercizi/Lezioni/02_11112025.c
+++ Materie/Informatica/Esercizi/Lezioni/02_11112025.c
@@ -1,29 +1,46 @@
-/* Cifratura */
+/* Data una stringa iniziale e il fattore di
+    cifratura creare la stringa cifrata */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 typedef char* string;
 
+cfrtr(string cfr, int n){
+    int len=strlen(cfr);
+    int i=0, cntr;
+
+    string tmp=malloc(len*sizeof(char));
+
+    if(tmp==NULL){
+        printf("Errore!");
+        exit(1);
+    }
+    while(cfr[i]!='\0'){
+        cntr=0;
+        if(cfr[i]+n){
+            
+        }
+    }
+
+    return tmp;
+}
+
 int main(){
 
-    string cfr;
-    int n,len;//,dim=20;
+    string cfr=(string)malloc(200*sizeof(char));
+    int n;
 
-    //cfr=(string)malloc((dim+1)*sizeof(char));
+    printf("\nInserisci una stringa: ");
+    fgets(cfr, 200, stdin);
 
-    printf("\nInserisci una parola: ");
-    fgets(cfr, sizeof(cfr), stdin);
-
-    len=strlen(cfr);
     //cfr=(string)realloc(dim,(len+1)*sizeof(char));
 
     printf("\nInserisci una cifra: ");
     scanf("%d", &n);
 
-    for(int i=0; i<len; i++){
-        cfr[i]+=n;
-    }
+    cfr=cfrtr(cfr,n);
+
 
     printf("\n Ecco la parola cifrata. \n %s", cfr);
 
