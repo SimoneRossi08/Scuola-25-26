8bc77a9e373419494273d45f873e99e0f01d8b5e
Simone Rossi <simone.rossi@galileiostiglia.edu.it>
2026-04-27 11:56:21 +0000

    27/04/2026

---
+++ Materie/Informatica/file.c
@@ -0,0 +1,11 @@
+
+FILE *f = fopen("dati.txt", "r");
+ftell(f);
+rewind(f);
+fwrite(&n, sizeof(int), 1, f);
+fread(&n, sizeof(int), 1, f);
+fseek(f, offset, SEEK_SET);
+fgetc(FILE *f);
+fputc(int c, FILE *f);
+fprintf(f, "%d\n", n);
+fscanf(f, "%d", &n);
